7a41fc583cba18c38647474af5d9b9636ed6720a
jgraykeyin <justin.gray@keyin.com>
2021-01-19 17:23:17 -0330

    Inputs are now being processed

--- dom_nlchoc.js
+++ dom_nlchoc.js
@@ -10,14 +10,46 @@ function getFormData() {
     let number_days = document.getElementById("number_days").value;
     let car_status = document.getElementById("car_status").value;
 
-    // Just print it back to the console for now
-    console.log(emp_number);
-    console.log(emp_name);
-    console.log(location);
-    console.log(start_date);
-    console.log(end_date);
-    console.log(number_days);
-    console.log(car_status);
+    let total_km = 0;
+    if (car_status.toUpperCase() == "O") {
+        total_km = prompt("Total kilometers: ");
+    }
+    
+    // Calculate the per diem based on the number of days of the trip
+    let perdiem = Number;
+    if (number_days <= 3) {
+        perdiem = number_days * 85;
+    } else {
+        perdiem = number_days * 100;
+    }
+    
+    // Calculate the mileage based on if the car is rented or owned
+    let mileage = Number;
+    if (car_status == "O") {
+        mileage = total_km * 0.10;
+    } else if (car_status == "R") {
+        mileage = number_days * 56;
+    }
+    
+    // Calculate the claim amount, taxes and totals
+    let claim_amount = perdiem + mileage;
+    let hst = claim_amount * 0.13;
+    let claim_total = claim_amount + hst;
+    
+    // Print the output!
+    console.log(`Employee Number: ${emp_number}`);
+    console.log(`Employee Name: ${emp_name}`);
+    console.log(`Trip Location: ${location}`);
+    console.log(`Start Date: ${start_date}`);
+    console.log(`End Date: ${end_date}`);
+    console.log(`Number of days: ${number_days}`);
+    console.log(`Car Status: ${car_status}`);
+    console.log(`Per Diem: \$${perdiem}`);
+    console.log(`Mileage: ${mileage} km`);
+    console.log(`Claim Amount: \$${claim_amount}`);
+    console.log(`Taxes: \$${hst}`);
+    console.log(`Claim Total: \$${claim_total}`);
+    
 }
 
 

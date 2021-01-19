489794737b838e37e17825f4f7b7a431410d6735
jgraykeyin <justin.gray@keyin.com>
2021-01-19 17:20:46 -0330

    Added input fields into a table

--- dom_nlchoc.js
+++ dom_nlchoc.js
@@ -1,13 +1,23 @@
 function getFormData() {
     // This function is triggered by the button in our html document.
-    // Right now it just grabs a single value from an input but
-    // we can add all other inputs in the function too.
 
-    // Get the value from the html element with id emp_number
+    // Get the values from all the inputs
     let emp_number = document.getElementById("emp_number").value;
+    let emp_name = document.getElementById("emp_name").value;
+    let location = document.getElementById("location").value;
+    let start_date = document.getElementById("start_date").value;
+    let end_date = document.getElementById("end_date").value;
+    let number_days = document.getElementById("number_days").value;
+    let car_status = document.getElementById("car_status").value;
 
     // Just print it back to the console for now
     console.log(emp_number);
+    console.log(emp_name);
+    console.log(location);
+    console.log(start_date);
+    console.log(end_date);
+    console.log(number_days);
+    console.log(car_status);
 }
 
 

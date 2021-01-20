235b3cddf48b0c853d224882f8997b80a664887e
jgraykeyin <justin.gray@keyin.com>
2021-01-20 09:12:05 -0330

    Van Tracker setup to work in the console without any HTML

--- vantracker.js
+++ vantracker.js
@@ -1,14 +1,25 @@
+// Import a nodejs module to allow user inputs in the console.
+// To install this module use: npm install prompt-sync
+const prompt = require('prompt-sync')();
+
 console.log("Top Secret Van Tracker");
 
-function getFormData() {
-    let track_vans = true;
-    let license_plate = document.getElementById("license_plate").value;
-    let vin_number = document.getElementById("vin_number").value;
-    let company = document.getElementById("company").value;
-    let model = document.getElementById("model").value;
-    let cargo = document.getElementById("cargo").value;
-    
-    let vehicle_info = [];
+let track_vans = true;
+let license_plate = String;
+let vin_number = String;
+let company = String;
+let model = String;
+let cargo = Number;
+let tracking_status = String;
+let vehicle_info = [];
+
+while (track_vans == true) {
+
+    license_plate = prompt("License plate: ");
+    vin_number = prompt("VIN: ");
+    company = prompt("Company: ");
+    model = prompt("Model: ");
+    cargo = prompt("Cargo capacity in cubic cm: ");
 
     // Append the vehicle info object with the new information from inputs
     vehicle_info.push({
@@ -21,6 +32,11 @@ function getFormData() {
         }
     });
 
-    console.table(vehicle_info);
+    tracking_status = prompt("Would you like to enter another vehicle? (Y or N) ");
+    if (tracking_status.toUpperCase() == "N") {
+        break
+    }
+
 }
 
+console.log(vehicle_info);

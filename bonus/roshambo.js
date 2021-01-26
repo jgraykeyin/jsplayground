8ea27c3a47deb72ecd07b5de7a3e4c457228d2ce
jgraykeyin <justin.gray@keyin.com>
2021-01-25 21:38:17 -0330

    Added a scoreboard

--- bonus/roshambo.js
+++ bonus/roshambo.js
@@ -8,14 +8,16 @@ function play_game() {
 
     // Get the user-selected action
     let player_move = document.querySelector("#player_action").value;
-    console.log(player_move);
 
     // Create an array of possible actions
     let actions = ["rock","paper","scissor"];
 
     // Select a random action from the array
     let cpu_move = actions[Math.floor(Math.random() * actions.length)];
-    console.log(cpu_move);
+
+    // Get the current game score
+    let player_score = parseInt(document.querySelector("#player_score").innerHTML);
+    let cpu_score = parseInt(document.querySelector("#cpu_score").innerHTML);
 
     // Figure out who won, who lost or if it's a tie.
     // This can probably be improved with a function...or something.
@@ -26,20 +28,26 @@ function play_game() {
         if (player_move == "rock") {
             if (cpu_move == "paper") {
                 status = "CPU WINS!";
+                cpu_score+=1;
             } else if (cpu_move == "scissor") {
                 status = "PLAYER WINS!";
+                player_score+=1;
             }
         } else if (player_move == "paper") {
             if (cpu_move == "rock") {
                 status = "PLAYER WINS";
+                player_score+=1;
             } else if (cpu_move == "scissor") {
                 status = "CPU WINS";
+                cpu_score+=1;
             }
         } else if (player_move == "scissor") {
             if (cpu_move == "rock") {
                 status = "CPU WINS!";
+                cpu_score+=1;
             } else if (cpu_move = "paper") {
                 status = "PLAYER WINS!"
+                player_score+=1;
             }
         }
     }
@@ -52,7 +60,10 @@ function play_game() {
     // Insert our new HTML code into the content tag of our document
     document.querySelector("#results").innerHTML = html;
 
-}
+    // Now we update the score in our document
+    document.querySelector("#player_score").innerHTML = player_score;
+    document.querySelector("#cpu_score").innerHTML = cpu_score;
 
+}
 
 window.onload = main;
